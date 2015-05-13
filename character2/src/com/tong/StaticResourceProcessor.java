aa1e54c576af068a3a95b0dac442de761c468491
sishen123258 <1144299328@qq.com>
2015-05-13 19:12:51 +0800

    tomcat

--- character2/src/com/tong/StaticResourceProcessor.java
+++ character2/src/com/tong/StaticResourceProcessor.java
@@ -3,12 +3,18 @@ package com.tong;
 import com.tong.connector.http.HttpRequest;
 import com.tong.connector.http.HttpResponse;
 
+import java.io.IOException;
+
 /**
  * Created by Tong on 2015/5/13.
  */
 public class StaticResourceProcessor {
 
     public void process(HttpRequest request, HttpResponse response) {
-        response.sendStaticResources();
+        try {
+            response.sendStaticResource();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- character2/src/com/tong/startup/Bootstrap.java
+++ character2/src/com/tong/startup/Bootstrap.java
@@ -1,5 +1,6 @@
 package com.tong.startup;
 
+import com.tong.connector.http.Constants;
 import com.tong.connector.http.HttpConnector;
 
 /**
@@ -8,6 +9,7 @@ import com.tong.connector.http.HttpConnector;
 public final class Bootstrap {
 
     public static void main(String[] args) {
+        System.out.println("Bootstrap.main:"+ Constants.WEB_ROOT);
         HttpConnector httpConnector=new HttpConnector();
         httpConnector.start();
     }
