6d7138aa496f558392d66251789814123d90d9fb
sishen123258 <1144299328@qq.com>
2015-05-17 11:44:34 +0800

    tomcat

---
+++ character3/src/core/SimpleContainer.java
@@ -0,0 +1,178 @@
+package core;
+
+import org.apache.catalina.*;
+
+import javax.naming.directory.DirContext;
+import javax.servlet.ServletException;
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+
+/**
+ * Created by Tong on 2015/5/17.
+ */
+public  class SimpleContainer implements Container{
+    @Override
+    public void addChild(Container container) {
+
+    }
+
+    @Override
+    public void addContainerListener(ContainerListener containerListener) {
+
+    }
+
+    @Override
+    public void addMapper(Mapper mapper) {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+
+    }
+
+    @Override
+    public Container findChild(String s) {
+        return null;
+    }
+
+    @Override
+    public Container[] findChildren() {
+        return new Container[0];
+    }
+
+    @Override
+    public Mapper findMapper(String s) {
+        return null;
+    }
+
+    @Override
+    public Mapper[] findMappers() {
+        return new Mapper[0];
+    }
+
+    @Override
+    public Cluster getCluster() {
+        return null;
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public Loader getLoader() {
+        return null;
+    }
+
+    @Override
+    public Logger getLogger() {
+        return null;
+    }
+
+    @Override
+    public Manager getManager() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Container getParent() {
+        return null;
+    }
+
+    @Override
+    public ClassLoader getParentClassLoader() {
+        return null;
+    }
+
+    @Override
+    public Realm getRealm() {
+        return null;
+    }
+
+    @Override
+    public DirContext getResources() {
+        return null;
+    }
+
+    @Override
+    public void invoke(Request request, Response response) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public Container map(Request request, boolean b) {
+        return null;
+    }
+
+    @Override
+    public void removeChild(Container container) {
+
+    }
+
+    @Override
+    public void removeContainerListener(ContainerListener containerListener) {
+
+    }
+
+    @Override
+    public void removeMapper(Mapper mapper) {
+
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+
+    }
+
+    @Override
+    public void setCluster(Cluster cluster) {
+
+    }
+
+    @Override
+    public void setLoader(Loader loader) {
+
+    }
+
+    @Override
+    public void setLogger(Logger logger) {
+
+    }
+
+    @Override
+    public void setManager(Manager manager) {
+
+    }
+
+    @Override
+    public void setName(String s) {
+
+    }
+
+    @Override
+    public void setParent(Container container) {
+
+    }
+
+    @Override
+    public void setParentClassLoader(ClassLoader classLoader) {
+
+    }
+
+    @Override
+    public void setRealm(Realm realm) {
+
+    }
+
+    @Override
+    public void setResources(DirContext dirContext) {
+
+    }
+}
---
+++ character3/src/startup/Bootstrap.java
@@ -0,0 +1,15 @@
+package startup;
+
+import org.apache.catalina.connector.http.HttpConnector;
+
+/**
+ * Created by Tong on 2015/5/17.
+ */
+public final class Bootstrap {
+
+    public static void main(String[] args) {
+        HttpConnector connector=new HttpConnector();
+        SimpleCon
+    }
+
+}
